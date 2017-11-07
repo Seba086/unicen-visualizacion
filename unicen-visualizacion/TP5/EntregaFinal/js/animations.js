1c0513874ecc08a399266c20045ae8e85d95c8e4
Seba <nfseba@gmail.com>
2017-11-07 16:16:30 -0300

    Se agrega opacidad a la grilla de Tweets. Se agregan botones para switchear entre layouts

--- unicen-visualizacion/TP5/EntregaFinal/js/animations.js
+++ unicen-visualizacion/TP5/EntregaFinal/js/animations.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
 	document.getElementById("likeHeart").addEventListener("click", likeChangeState);
-
+	document.getElementById("grid").addEventListener("click", setGridActive);
+	document.getElementById("gallery").addEventListener("click", setGalleryActive);
 	function likeChangeState(){
 		this.className = "likedHeart";
 	}
+
+	function setGridActive(){
+		this.className = "material-icons viewMode active";
+		document.getElementById("gallery").className = "material-icons viewMode";
+	}
+	function setGalleryActive(){
+		this.className = "material-icons viewMode active";
+		document.getElementById("grid").className = "material-icons viewMode";
+	}	
+	setGridActive();
 });
